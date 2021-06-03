cc59cf1ddc780cf99c9ab49a20f976ea117687a4
John Yevsukov <github.com@john.engineering>
2021-06-03 18:01:56 -0400

    wip

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5000
 
 console.log('port is -> ', PORT)
 
+server.get('/', (req, res) => {
+    res.send(`<h2>go to /api</h2>`);
+  });
+
 server.get('/api', (req, res) => {
   res.json({ message: `hello there` })
 })
